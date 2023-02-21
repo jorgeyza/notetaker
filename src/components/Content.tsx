4cb2bf038f85acacb941789d9737d21366d8338e
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-21 17:09:55 -0500

    Can fetch and create topics

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,15 +1,64 @@
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 
-import { api } from "~/utils/api";
+import { api, RouterOutputs } from "~/utils/api";
+
+type Topic = RouterOutputs["topic"]["getAll"][0];
 
 export const Content: React.FC = () => {
   const { data: sessionData } = useSession();
+  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
 
   const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
     undefined, // no input
     {
       enabled: sessionData?.user !== undefined,
+      onSuccess: (data) => {
+        setSelectedTopic(selectedTopic ?? data[0] ?? null);
+      },
     }
   );
-  return <main>{JSON.stringify(topics)}</main>;
+
+  const createTopic = api.topic.create.useMutation({
+    onSuccess: () => {
+      void refetchTopics();
+    },
+  });
+
+  return (
+    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <div className="px-2">
+        <div className="divider" />
+        <ul className="menu rounded-box w-56 bg-base-100 p-2">
+          {topics?.map((topic) => (
+            <li key={topic.id}>
+              <a
+                href="#"
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  setSelectedTopic(topic);
+                }}
+              >
+                {topic.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          placeholder="New Topic"
+          className="input-bordered input input-sm w-full"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              createTopic.mutate({
+                title: e.currentTarget.value,
+              });
+              e.currentTarget.value = "";
+            }
+          }}
+        />
+      </div>
+      <div className="col-span-3"></div>
+    </div>
+  );
 };
