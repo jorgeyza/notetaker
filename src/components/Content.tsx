05ea87456bf8faad2cccf65fc5195cea45dfd4c6
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-21 18:05:31 -0500

    Add NoteCard

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,8 +1,10 @@
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 
-import { api, RouterOutputs } from "~/utils/api";
+import { NoteCard } from "./NoteCard";
 import { NoteEditor } from "./NoteEditor";
+
+import { api, RouterOutputs } from "~/utils/api";
 
 type Topic = RouterOutputs["topic"]["getAll"][0];
 
@@ -41,9 +43,15 @@ export const Content: React.FC = () => {
     },
   });
 
+  const deleteNote = api.note.delete.useMutation({
+    onSuccess: () => {
+      refetchNotes();
+    },
+  });
+
   return (
-    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
-      <div className="px-2">
+    <main className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <section className="px-2">
         <div className="divider" />
         <ul className="menu rounded-box w-56 bg-base-100 p-2">
           {topics?.map((topic) => (
@@ -73,8 +81,18 @@ export const Content: React.FC = () => {
             }
           }}
         />
-      </div>
-      <div className="col-span-3">
+      </section>
+      <section className="col-span-3">
+        <ul>
+          {notes?.map((note) => (
+            <li key={note.id} className="mt-5">
+              <NoteCard
+                note={note}
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+              />
+            </li>
+          ))}
+        </ul>
         <NoteEditor
           onSave={({ title, content }) => {
             createNote.mutate({
@@ -84,7 +102,7 @@ export const Content: React.FC = () => {
             });
           }}
         />
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
---
+++ src/components/NoteCard.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+import ReactMarkdown from "react-markdown";
+
+import { type RouterOutputs } from "../utils/api";
+
+type Note = RouterOutputs["note"]["getAll"][0];
+
+export const NoteCard = ({
+  note,
+  onDelete,
+}: {
+  note: Note;
+  onDelete: () => void;
+}) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(true);
+
+  return (
+    <article className="card mt-5 border border-gray-200 bg-base-100 shadow-xl">
+      <div className="card-body m-0 p-3">
+        <div
+          className={`collapse-arrow ${
+            isExpanded ? "collapse-open" : ""
+          } collapse`}
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          <div className="collapse-title text-xl font-bold">{note.title}</div>
+          <div className="collapse-content">
+            <div className="lg:prose-xl prose">
+              <ReactMarkdown>{note.content}</ReactMarkdown>
+            </div>
+          </div>
+        </div>
+        <div className="card-actions mx-2 flex justify-end">
+          <button className="btn-warning btn-xs btn px-5" onClick={onDelete}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </article>
+  );
+};
